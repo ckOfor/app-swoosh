bfdb9b341d73f743331c9d19bcdacfd8db77d82d
“Chinedu <chinedu@zebranow.com>
2020-01-27 19:17:15 +0100

    add models

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Ofor Chinedu on 27/01/2020.
+//  Copyright © 2020 Symple Innovations Company Limited. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
