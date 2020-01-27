2e0bc17448fbfdd6bf1686e17f1eb21a92678677
“Chinedu <chinedu@zebranow.com>
2020-01-27 18:10:29 +0100

    add forward and backward navigation

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -16,5 +16,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func unwindFromSKillVc(unwindSegue: UIStoryboardSegue) {
+        
+    }
 }
 
