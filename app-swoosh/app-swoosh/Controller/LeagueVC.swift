d6d5681a2ef36355f20efd5a1415af5b26572e4e
“Chinedu <chinedu@zebranow.com>
2020-01-27 18:53:48 +0100

    add segues

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
 
 }
